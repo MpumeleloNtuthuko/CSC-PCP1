ce0ae9b690add42eb2d7ec46f4d1bfe2e1fc841c
Mpumelelo Mpanza <112443193+MpumeleloNtuthuko@users.noreply.github.com>
2025-08-24 23:57:17 +0200

    Added import libraries, defined some attributes

--- src/DungeonMapParallel.java
+++ src/DungeonMapParallel.java
@@ -4,3 +4,22 @@
  *
  * M. Kuttel (2025) – adapted for parallel assignment
  */
+
+import java.util.Random;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class DungeonMapParallel {
+
+    public static final int PRECISION = 10000;
+    public static final int RESOLUTION = 5;
+
+    private int rows, columns;
+    private double xmin, xmax, ymin, ymax;
+    private int [][] manaMap;
+    private int [][] visit;
+    private int dungeonGridPointsEvaluated;
+    private double bossX;
+    private double bossY;
